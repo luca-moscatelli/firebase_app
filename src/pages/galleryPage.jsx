039a6ee2430e9ee6b387f57534d3c6bdd3147914
luca.moscatelli97 <luca.moscatelli97@gmail.com>
2022-08-21 16:24:06 +0200

    add url input

--- src/pages/galleryPage.jsx
+++ src/pages/galleryPage.jsx
@@ -4,16 +4,16 @@ import { useRef } from "react";
 import { useEffect } from "react";
 import { database } from "..";
 import NavigationHeader from "../navigation/MainNavigation";
-import { db_addPhoto, db_getAllPhoto, userPath } from "../utils/dbCalls";
+import { db_addPhoto , userPath } from "../utils/dbCalls";
 import styles from './galleryPage.module.css'
 
 function GalleryPage() {
     const [galleryDoc, setGalleryDoc] = useState([]);
 
+    const inputUrlRef=useRef();
+
     useEffect(() => {
-        getDocs(collection(database, userPath(), "gallery")).then((snap) =>
-            setGalleryDoc(snap.docs)
-        );
+       refreshPage();
     }, []);
 
     function refreshPage() {
@@ -33,10 +33,11 @@ function GalleryPage() {
         <div style={{ width: "full", height: "full" }}>
             <NavigationHeader />
             <div>
+                <input type={"url"} ref={inputUrlRef}></input>
                 <button
                     onClick={() => {
                         db_addPhoto(
-                            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1280px-Colosseo_2020.jpg"
+                            inputUrlRef.current.value
                         ).then(refreshPage);
                     }}
                 >
